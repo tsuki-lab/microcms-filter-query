0bfe587ae562e3be840f3682f8326cd2f2ecc39c
hanetsuki <48175599+tsuki-lab@users.noreply.github.com>
2022-04-02 10:52:57 +0900

    Add source files [ index.ts, type.ts, index.spec.ts]

---
+++ src/index.spec.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it } from "vitest";
+import { MicroCMSFilterQuery } from "./index";
+
+describe("equals", () => {
+  it("name[equals]John", () => {
+    const result = new MicroCMSFilterQuery().equals('name', 'John').$execute()
+
+    expect(result).toBe('name[equals]John');
+  });
+});
+
+describe("contains", () => {
+  it("category[contains]1234567", () => {
+    const result = new MicroCMSFilterQuery().contains('category', '1234567').$execute()
+
+    expect(result).toBe('category[contains]1234567');
+  });
+});
---
+++ src/type.ts
@@ -0,0 +1,25 @@
+type FilterQueryValue = (fieldId: string, value: string) => ConnectFn
+type FilterQueryNotValue = (fieldId: string) => ConnectFn
+
+type MicroCMSFilterQueryType = {
+  equals: FilterQueryValue
+  notEquals: FilterQueryValue
+  lessThan: FilterQueryValue
+  greaterThan: FilterQueryValue
+  contains: FilterQueryValue
+  beginsWith: FilterQueryValue
+  notExists: FilterQueryNotValue
+  exists: FilterQueryNotValue
+}
+
+type ConnectFn = {
+  and: () => AndOrFn
+  or: () => AndOrFn
+  $execute: () => string
+}
+
+type AndOrFn = MicroCMSFilterQueryType & {
+  _parentheses: (query: string) => ConnectFn
+}
+
+export { MicroCMSFilterQueryType }
