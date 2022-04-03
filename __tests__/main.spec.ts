578770c173b1d7197787d7084f2dda6d6bf3b5cf
hanetsuki <48175599+tsuki-lab@users.noreply.github.com>
2022-04-03 18:39:00 +0900

    Formatted files

--- __tests__/main.spec.ts
+++ __tests__/main.spec.ts
@@ -1,66 +1,78 @@
-import { describe, expect, it } from "vitest";
-import { MicroCMSFilterQuery } from "../src/main";
+import { describe, expect, it } from 'vitest'
+import { MicroCMSFilterQuery } from '../src/main'
 
-describe("`equals`メソッドの動作テスト", () => {
-  it("gender[equals]women", () => {
-    const result = new MicroCMSFilterQuery().equals('gender', 'women').$execute()
+describe('`equals`メソッドの動作テスト', () => {
+  it('gender[equals]women', () => {
+    const result = new MicroCMSFilterQuery()
+      .equals('gender', 'women')
+      .$execute()
 
-    expect(result).toBe('gender[equals]women');
-  });
-});
+    expect(result).toBe('gender[equals]women')
+  })
+})
 
-describe("`notEquals`メソッドの動作テスト", () => {
-  it("gender[not_equals]women", () => {
-    const result = new MicroCMSFilterQuery().notEquals('gender', 'women').$execute()
+describe('`notEquals`メソッドの動作テスト', () => {
+  it('gender[not_equals]women', () => {
+    const result = new MicroCMSFilterQuery()
+      .notEquals('gender', 'women')
+      .$execute()
 
-    expect(result).toBe('gender[not_equals]women');
-  });
-});
+    expect(result).toBe('gender[not_equals]women')
+  })
+})
 
-describe("`lessThan`メソッドの動作テスト", () => {
-  it("createdAt[less_than]2019-11", () => {
-    const result = new MicroCMSFilterQuery().lessThan('createdAt', '2019-11').$execute()
+describe('`lessThan`メソッドの動作テスト', () => {
+  it('createdAt[less_than]2019-11', () => {
+    const result = new MicroCMSFilterQuery()
+      .lessThan('createdAt', '2019-11')
+      .$execute()
 
-    expect(result).toBe('createdAt[less_than]2019-11');
-  });
-});
+    expect(result).toBe('createdAt[less_than]2019-11')
+  })
+})
 
-describe("`greaterThan`メソッドの動作テスト", () => {
-  it("createdAt[greater_than]2019-10", () => {
-    const result = new MicroCMSFilterQuery().greaterThan('createdAt', '2019-10').$execute()
+describe('`greaterThan`メソッドの動作テスト', () => {
+  it('createdAt[greater_than]2019-10', () => {
+    const result = new MicroCMSFilterQuery()
+      .greaterThan('createdAt', '2019-10')
+      .$execute()
 
-    expect(result).toBe('createdAt[greater_than]2019-10');
-  });
-});
+    expect(result).toBe('createdAt[greater_than]2019-10')
+  })
+})
 
-describe("`contains`メソッドの動作テスト", () => {
-  it("title[contains]おすすめ", () => {
-    const result = new MicroCMSFilterQuery().contains('title', 'おすすめ').$execute()
+describe('`contains`メソッドの動作テスト', () => {
+  it('title[contains]おすすめ', () => {
+    const result = new MicroCMSFilterQuery()
+      .contains('title', 'おすすめ')
+      .$execute()
 
-    expect(result).toBe('title[contains]おすすめ');
-  });
-});
+    expect(result).toBe('title[contains]おすすめ')
+  })
+})
 
-describe("`exists`メソッドの動作テスト", () => {
-  it("nextLink[exists]", () => {
+describe('`exists`メソッドの動作テスト', () => {
+  it('nextLink[exists]', () => {
     const result = new MicroCMSFilterQuery().exists('nextLink').$execute()
 
-    expect(result).toBe('nextLink[exists]');
-  });
-});
+    expect(result).toBe('nextLink[exists]')
+  })
+})
 
-describe("`notExists`メソッドの動作テスト", () => {
-  it("nextLink[not_exists]", () => {
+describe('`notExists`メソッドの動作テスト', () => {
+  it('nextLink[not_exists]', () => {
     const result = new MicroCMSFilterQuery().notExists('nextLink').$execute()
 
-    expect(result).toBe('nextLink[not_exists]');
-  });
-});
+    expect(result).toBe('nextLink[not_exists]')
+  })
+})
 
-describe("`beginsWith`メソッドの動作テスト", () => {
-  it("publishedAt[begins_with]2019-11", () => {
-    const result = new MicroCMSFilterQuery().beginsWith('publishedAt', '2019-11').$execute()
+describe('`beginsWith`メソッドの動作テスト', () => {
+  it('publishedAt[begins_with]2019-11', () => {
+    const result = new MicroCMSFilterQuery()
+      .beginsWith('publishedAt', '2019-11')
+      .$execute()
 
-    expect(result).toBe('publishedAt[begins_with]2019-11');
-  });
-});
+    expect(result).toBe('publishedAt[begins_with]2019-11')
+  })
+})
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
-import { ConjunctFunction, MicroCMSFilterQueryType } from "./types"
+import { ConjunctFunction, MicroCMSFilterQueryType } from './types'
 
+/**
+ * create to `filters` query params for microcms
+ *
+ * @export
+ * @class MicroCMSFilterQuery
+ * @implements {MicroCMSFilterQueryType}
+ */
 export class MicroCMSFilterQuery implements MicroCMSFilterQueryType {
   private _query = ''
 
   private connectFn = {
-    and:() => {
+    and: () => {
       this._query += `[and]`
       return this
     },
@@ -14,49 +21,118 @@ export class MicroCMSFilterQuery implements MicroCMSFilterQueryType {
     },
     $execute: () => {
       return this._query
-    }
+    },
   }
 
+  /**
+   * add query for `equals`
+   *
+   * @param {string} fieldId
+   * @param {string} value
+   * @memberof MicroCMSFilterQuery
+   * @return {ConjunctFunction}
+   */
   public equals = (fieldId: string, value: string): ConjunctFunction => {
     this._query += `${fieldId}[equals]${value}`
     return this.connectFn
   }
 
+  /**
+   * add query for `not_equals`
+   *
+   * @param {string} fieldId
+   * @param {string} value
+   * @memberof MicroCMSFilterQuery
+   * @return {ConjunctFunction}
+   */
   public notEquals = (fieldId: string, value: string): ConjunctFunction => {
     this._query += `${fieldId}[not_equals]${value}`
     return this.connectFn
   }
 
+  /**
+   * add query for `less_than`
+   *
+   * @param {string} fieldId
+   * @param {string} value
+   * @memberof MicroCMSFilterQuery
+   * @return {ConjunctFunction}
+   */
   public lessThan = (fieldId: string, value: string): ConjunctFunction => {
     this._query += `${fieldId}[less_than]${value}`
     return this.connectFn
   }
 
+  /**
+   * add query for `greater_than`
+   *
+   * @param {string} fieldId
+   * @param {string} value
+   * @memberof MicroCMSFilterQuery
+   * @return {ConjunctFunction}
+   */
   public greaterThan = (fieldId: string, value: string): ConjunctFunction => {
     this._query += `${fieldId}[greater_than]${value}`
     return this.connectFn
   }
 
+  /**
+   * add query for `contains`
+   *
+   * @param {string} fieldId
+   * @param {string} value
+   * @memberof MicroCMSFilterQuery
+   * @return {ConjunctFunction}
+   */
   public contains = (fieldId: string, value: string): ConjunctFunction => {
     this._query += `${fieldId}[contains]${value}`
     return this.connectFn
   }
 
+  /**
+   * add query for `begins_with`
+   *
+   * @param {string} fieldId
+   * @param {string} value
+   * @memberof MicroCMSFilterQuery
+   * @return {ConjunctFunction}
+   */
   public beginsWith = (fieldId: string, value: string): ConjunctFunction => {
     this._query += `${fieldId}[begins_with]${value}`
     return this.connectFn
   }
 
+  /**
+   * add query for `not_exists`
+   *
+   * @param {string} fieldId
+   * @memberof MicroCMSFilterQuery
+   * @return {ConjunctFunction}
+   */
   public notExists = (fieldId: string): ConjunctFunction => {
     this._query += `${fieldId}[not_exists]`
     return this.connectFn
   }
 
+  /**
+   * add query for `exists`
+   *
+   * @param {string} fieldId
+   * @memberof MicroCMSFilterQuery
+   * @return {ConjunctFunction}
+   */
   public exists = (fieldId: string): ConjunctFunction => {
     this._query += `${fieldId}[exists]`
     return this.connectFn
   }
 
+  /**
+   *  add to arg `query` between `(`,`)`
+   *
+   * @param {string} query
+   * @memberof MicroCMSFilterQuery
+   * @return {ConjunctFunction}
+   */
   public _parentheses = (query: string): ConjunctFunction => {
     this._query += `(${query})`
     return this.connectFn
