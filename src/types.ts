1721e0ee94c3aca55c6d0a047d88c49707304ca9
hanetsuki <48175599+tsuki-lab@users.noreply.github.com>
2022-04-06 12:40:48 +0900

    Add `type-fest`

--- src/types.ts
+++ src/types.ts
@@ -1,27 +1,27 @@
+import { UnionToIntersection } from 'type-fest'
+
 type ConvertObjectToPrimitive<T extends Exclude<object, null>> = {
   [K in keyof T]: T[K] extends Exclude<object, null> ? string : T[K]
-};
+}
 
 type PrependProperty<T extends Exclude<object, null>, P extends string> = {
   [K in Extract<keyof T, string> as `${P}.${K}`]: T[K]
-};
+}
 
 type ProcessObject<T extends Exclude<object, null>> = {
-  [K in Extract<keyof T, string> as T[K] extends Exclude<object, null> ? K : never]: T[K] extends Exclude<object, null> ? PrependProperty<InternalFilterQuery<T[K]>, K> : never;
-};
+  [K in Extract<keyof T, string> as T[K] extends Exclude<object, null>
+    ? K
+    : never]: T[K] extends Exclude<object, null>
+    ? PrependProperty<InternalFilterQuery<T[K]>, K>
+    : never
+}
 
-type UnionToIntersection<U> = (
-U extends unknown
-  ? (distributedUnion: U) => void
-  : never
-) extends ((mergedIntersection: infer Intersection) => void)
-  ? Intersection
-  : never;
-
-type InternalFilterQuery<T extends Exclude<object, null>> = ConvertObjectToPrimitive<T> & UnionToIntersection<ProcessObject<T>[keyof ProcessObject<T>]>;
+type InternalFilterQuery<T extends Exclude<object, null>> =
+  ConvertObjectToPrimitive<T> &
+    UnionToIntersection<ProcessObject<T>[keyof ProcessObject<T>]>
 
 type FilterQuery<T extends Exclude<object, null>> = {
   [K in keyof InternalFilterQuery<T>]: InternalFilterQuery<T>[K]
-};
+}
 
 export { FilterQuery }
